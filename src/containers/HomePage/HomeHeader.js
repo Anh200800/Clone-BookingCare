5c4a3c47c0c513e1b2d0430c0edf9b036abc81f7
Your Name <you@example.com>
2022-04-13 23:49:49 +0700

    nine commit

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import "./HomeHeader.scss";
 import logo from '../../assets/logo.svg';
-import {FormattedMessage} from 'react-intj'
+import {FormattedMessage} from 'react-intl';
 
 class HomeHeader extends Component {
   render() {
     return(
       <React.Fragment>
 
+
       <div className="home-header-container">
         <div className="home-header-content">
           <div className="left-content">
             <i className="fas fa-bars"></i>
-            <img className="header-logo" src={logo}></img>
+            <img className="header-logo" src={logo} />
 
           </div>
           <div className="center-content">
@@ -24,10 +25,9 @@ class HomeHeader extends Component {
             </div>
             
           <div className="child-content">
-          <div><b><FormattedMessage id="homeheader.health-facility"/></b></div>
+          <div><b><FormattedMessage id="homeheader.health-facility" /></b></div>
           <div className="subs-title"><FormattedMessage id="homeheader.select-room/></div>
-            
-          </div>
+            </div>
           <div className="child-content">
           <div><b><FormattedMessage id="homeheader.doctor"/></b></div>
           <div className="subs-title"><FormattedMessage id="homeheader.select-doctor/></div>
@@ -53,46 +53,53 @@ class HomeHeader extends Component {
       </div>
       <div className="home-header-banner">
         <div className="content-up">
-          <div className="title1">NỀN TẢNG Y TẾ</div>
-          <div className="title2">CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+          <div className="title1"><FormattedMessage id="banner.title1" /></div></div></div>
+          <div className="title2"><FormattedMessage id="banner.title2" /></div>
           <div className="search">
             <i className="fas fa-search"></i>
             <input type="text" placeholder="Tim chuyên khoa khám bệnh" />
           </div>
 
 
-        </div>
-        <div className="content-down">
+</div>
+<div className="content-down">
           <div className="options">
-            <div className="options-child">
+             <div className="options-child">
 
             <div className="icon-child"><i className="fas fa-hospital"></i></div>
-            <div className="text-child">Khám chuyên khoa</div>
+            <div className="text-child"><FormattedMessage id="banner.child1" /></div>
             </div>
             <div className="options-child">
 
             <div className="icon-child"><i className="fas fa-mobile-alt"></i></div>
-            <div className="text-child">Khám từ xa</div>
+            <div className="text-child"><FormattedMessage id="banner.child2" /></div>
             </div>
             <div className="options-child">
 
             <div className="icon-child"><i className="fas fa-procedures"></i></div>
-            <div className="text-child">Khám tổng quát</div>
+            <div className="text-child"><FormattedMessage id="banner.child3></div>
+            </div>
+            <div className="options-child">
+            
+            <div className="icon-child"><i className="fas fa-flask"></i></div>
+            <div className="text-child"><FormattedMessage id="banner.child4"/></div>
+            </div>
+            
+            <div className="options-child">
+
+            <div className="icon-child"><i className="fas fa-user-md"></i></div>
+            <div className="text-child"><FormattedMessage id="banner.child5></div>
             </div>
             <div className="options-child">
 
-            <div className="icon-child"><i className="fas fa-microscope"></i></div>
-            <div className="text-child">Xét nghiệm y học</div>
+            <div className="icon-child"><i className="fas fa-briefcase-medical"></i></div>
+            <div className="text-child"><FormattedMessage id="banner.child6></div>
             </div>
-            <div className="options-child">
-
-            <div className="icon-child"><i className="fas fa-tooth"></i></div>
-            <div className="text-child">Khám nha khoa</div>
             </div>
-          </div>
-        </div>
+            </div>
       </div>
-</React.Fragment>
+
+      </React.Fragment>
     )
   }
 }
